d7b58598eab2f5827bbd2b8e6a99fa5614a6dce2
abidurr <42abid@gmail.com>
2020-06-06 01:51:09 -0400

    Get specific post

--- routes/posts.js
+++ routes/posts.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+// get all posts
 router.get("/", async (req, res) => {
     try {
         const posts = await Post.find();
         res.status(200).json(posts);
     } catch (err) {
-        res.json({ msg: err })
+        res.json({ msg: err });
     }
 });
 
-router.get("/specific", (req, res) => res.send("Hello world posts/specific!"));
-
+// submit a post
 router.post("/", async (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -22,7 +22,17 @@ router.post("/", async (req, res) => {
         const savedPost = await post.save();
         res.status(200).json(savedPost);
     } catch (err) {
-        res.status(418).json({ msg: err })
+        res.status(418).json({ msg: err });
+    }
+});
+
+// get specific post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(418).json({ msg: err });
     }
 });
 
