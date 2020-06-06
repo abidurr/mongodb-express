3f5d4f5cfcf719d696706cfa009f144fc57378a6
abidurr <42abid@gmail.com>
2020-06-05 21:31:00 -0400

    Simple MongoDB connection

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get("/", (req, res) => res.send("Hello world!"));
 
-app.listen(3000);
+// connect mongoose
+mongoose.connect(
+    process.env.DB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("Connected to MongoDB!")
+);
+
+PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
